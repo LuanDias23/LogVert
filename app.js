ac6efcf6da55a4c8163678f16cf744502d8c75f9
Henrique <henrique.santos1707@gmail.com>
2025-09-30 22:14:16 -0300

    Ajuste de estrutura + tela de venda

--- app.js
+++ app.js
@@ -10,9 +10,25 @@ app.use(cp());
 app.use(express.static('public'));
 app.set('view engine', 'hbs');
 
-app.use('/lojista/', require('./routes/lojista/login'));
-app.use('/lojista/produtos', require('./routes/lojista/products'));
-app.use((req, res) => { res.redirect('/lojista/login'); });
+
+app.use('/lojista/produtos', (req, res) => {
+    res.render('produtos');
+});
+app.use('/lojista/login', (req, res) => {
+    res.render('login');
+});
+app.use('/lojista/vendas', (req, res) => {
+    res.render('vendas');
+});
+
+
+
+
+
+app.use('/lojista/', (req, res) => {
+    res.render('menu');
+});
+app.use((req, res) => { res.redirect('/lojista'); });
 
 console.log('\x1b[36m[INFO]: \x1b[0mNodeJS iniciado na porta 5500.');
 app.listen(5500, '0.0.0.0');
