a6d47fc41dff4c2d0806f5988f52e37b414b5a1a
Luan Dias <ldfacul1@gmail.com>
2025-11-10 10:36:39 -0300

    pag historico vendas, filtros e FAQ

--- LogVert-main/FAQ/faq.js
+++ LogVert-main/FAQ/faq.js
@@ -3,14 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (form) {
         form.addEventListener('submit', function(event) {
-            event.preventDefault();
+            event.preventDefault(); // Impede o envio real
 
+            // Pega os dados (apenas para exemplo)
             const formData = new FormData(form);
             const data = Object.fromEntries(formData.entries());
 
             console.log('Dados do formulário de Suporte:', data);
+            
+            // Simula o envio
             alert('Mensagem enviada com sucesso! Entraremos em contato em breve.');
             
+            // Limpa o formulário
             form.reset();
         });
     }
